6f956a5a1fb3924ca4ecd9ea5c9b28a8154f38de
Luisa Santos <luisacristina.sn@icloud.com>
2024-07-04 16:01:14 -0300

    .

---
+++ basico/tests.js
@@ -0,0 +1,36 @@
+// Códigos usados no console do Navegador
+
+window.alert('Opa')
+window.confirm('deseja apagar?') // pressionar ok retorna true, pressionar cancel retorna false
+window.prompt('Digite seu nome:' )
+
+
+
+let num1 = parseInt(prompt('Digite um número: '))
+let num2 = parseInt(prompt('Digite outro número: '))
+
+const resultado = num1 + num2
+alert(`O resultado é ${resultado}`)
+
+//Exercícios
+let varA = 'a' //b 
+let varB = 'b' //c
+let varC = 'c' //a
+
+let varAux = varA
+
+varA = varB
+varB = varC
+varC = varAux
+console.log(varA, varB, varC)
+
+
+
+//Outra Forma
+let varA = 'a'; //b 
+let varB = 'b'; //c
+let varC = 'c';//a
+
+[varA, varB, varC] = [varB, varC, varA]
+
+console.log(varA, varB, varC)
