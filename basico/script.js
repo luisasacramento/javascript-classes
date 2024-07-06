1909fd867dd37a6aacff745c7df591d569c77726
Luisa Santos <luisacristina.sn@icloud.com>
2024-07-05 23:41:24 -0300

    .

---
+++ basico/script.js
@@ -0,0 +1,8 @@
+/*
+Primitivos (imutáveis)- string, number, boolean, undefined, null, (bigint, symbol)
+- valores copiados
+
+Referência (mutável) - array, object, function
+- passados por referência
+
+*/
