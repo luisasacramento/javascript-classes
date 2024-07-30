f61e4ec326429c0afcfac66a791754a8aa26be42
Luisa Santos <luisacristina.sn@icloud.com>
2024-07-30 12:44:38 -0300

    .

---
+++ logica/object-Date.js
@@ -0,0 +1,20 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+
+
+function zeroAEsquerda(num){
+    return num >= 10 ? num : `0${num}`;
+}
+function formataData(data){
+    const dia = zeroAEsquerda(data.getDate())
+    const mes = zeroAEsquerda(data.getMonth())
+    const ano = zeroAEsquerda(data.getFullYear())
+    const hora = zeroAEsquerda(data.getHours())
+    const min = zeroAEsquerda(data.getMinutes())
+    const seg = zeroAEsquerda(data.getSeconds())
+
+    return `${dia}/${mes}/${ano} ${hora}:${min}:${seg}`
+}
+
+const data = new Date();
+const dataBrasil = formataData(data);
+console.log(dataBrasil);
